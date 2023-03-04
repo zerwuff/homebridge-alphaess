c2671cab3a9c4f0de7fb79882deceaee0b4c68ca
jenszeidler <jens.zeidler@dbfuhrpark.com>
2023-03-04 15:00:40 +0100

    fix test

--- src/alpha/response/AlphaDetailResponse.ts
+++ src/alpha/response/AlphaDetailResponse.ts
@@ -1,15 +1,6 @@
 
 import { JsonProperty, JsonClassType } from 'jackson-js';
 
-export class AlphaDetailResponse {
-
-  @JsonProperty() @JsonClassType({type: () => [Number]})
-    code: number;
-
-  @JsonProperty() @JsonClassType({type: () => [AlphaData]})
-    data: AlphaData;
-}
-
 export class AlphaData {
 
   @JsonProperty() @JsonClassType({type: () => [Number]})
@@ -58,4 +49,13 @@ export class AlphaData {
   // {"code":200,"info":"Success"
   // * ,"data":{"ppv1":0.0,"ppv2":0.0,"ppv3":0.0,"ppv4":0.0,"preal_l1":6.0,"preal_l2":8.0,"preal_l3":8.0,"pmeter_l1":207.0,"pmeter_l2":15.0,"pmeter_l3":119.0,"pmeter_dc":0.0,"soc":4.4,"pbat":0.0,"ev1_power":0,"ev2_power":0,"ev3_power":0,"ev4_power":0,"createtime":"2022-12-20 07:02:05","ups_model":0}}
 
+}
+
+export class AlphaDetailResponse {
+
+  @JsonProperty() @JsonClassType({type: () => [Number]})
+    code: number;
+
+  @JsonProperty() @JsonClassType({type: () => [AlphaData]})
+    data: AlphaData;
 }
--- src/alpha/response/AlphaLoginResponse.ts
+++ src/alpha/response/AlphaLoginResponse.ts
@@ -1,14 +1,7 @@
 
 import { JsonProperty, JsonClassType } from 'jackson-js';
 
-export class AlphaLoginResponse {
 
-  @JsonProperty() @JsonClassType({type: () => [Number]})
-    code: number;
-
-  @JsonProperty() @JsonClassType({type: () => [LoginReponse]})
-    data: LoginReponse;
-}
 
 export class LoginReponse {
 
@@ -23,4 +16,13 @@ export class LoginReponse {
 
   //  Body :{"code":200,"info":"Success","data":{"AccessToken":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJaZWlkbGVyIiwiVXNlck5hbWUiOiJaZWlkbGVyIiwiVXNlcktleSI6IjE0ODQ5IiwiUmVmcmVzaFRva2VuS2V5IjoiM2U0OTY2ODQtYzk1Yy00OWViLWFjMTYtZWYxMDI0OGE5ZThhIiwiTGFuZ3VhZ2VDb2RlIjoiZGUtREUiLCJVc2VyVHlwZXMiOiJjdXN0b21lciIsIlVzZXJMaWNubyI6IiIsIlVzZXJDb250cm9sTmFtZSI6IiIsIklzT0VNVXNlciI6IjAiLCJVc2VyR3JvdXBOYW1lIjoiIiwiVXNlckdyb3VwSWQiOiIwIiwiQWRkaXRpb25hbEZpZWxkcyI6IiIsIkFkZGl0aW9uYWxGaWVsZHMyIjoiIiwibmJmIjoxNjcxNjAzODM2LCJleHAiOjE2NzE2NDM0MzYsImlzcyI6IkFscGhhQ2xvdWQiLCJhdWQiOiJBbHBoYUNsb3VkIn0.YgSJ8NRYQ7LbLwnY2--XoAoOuGDRpAJrje3_VSsbUGs","ExpiresIn":36000,"TokenCreateTime":"2022/12/21 上午7:23:56","RefreshTokenKey":"3e496684-c95c-49eb-ac16-ef10248a9e8a"}}
 
+}
+
+export class AlphaLoginResponse {
+
+  @JsonProperty() @JsonClassType({type: () => [Number]})
+    code: number;
+
+  @JsonProperty() @JsonClassType({type: () => [LoginReponse]})
+    data: LoginReponse;
 }
