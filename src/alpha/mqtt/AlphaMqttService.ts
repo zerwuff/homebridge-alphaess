03352e262562c37ddbbf39d6839735ba40cb2e11
jenszeidler <jens.zeidler@dbfuhrpark.com>
2023-05-17 18:22:44 +0200

    Added trigger debug message

--- src/alpha/mqtt/AlphaMqttService.ts
+++ src/alpha/mqtt/AlphaMqttService.ts
@@ -58,12 +58,19 @@ export class AlphaMqttService {
     this.log.debug('trying to connect to mqtt to send trigger message');
     const client = connect(this.mqtt_url, {clientId:this.name});
     client.on('connect', ()=> {
+      this.log.debug('connected to mqtt');
+
       if (trigger===true){
+        this.log.debug('sending message trigger true: ' + this.topics.mqtt_trigger_message_true + 'on topic: ' + this.topics.mqtt_trigger_topic_true);
         client.publish(this.topics.mqtt_trigger_topic_true, this.topics.mqtt_trigger_message_true);
-      }else{
+        client.end();
+        return;
+      } else {
+        this.log.debug('sending message trigger false: ' + this.topics.mqtt_trigger_message_false + 'on topic: ' + this.topics.mqtt_trigger_topic_false);
         client.publish(this.topics.mqtt_trigger_topic_false, this.topics.mqtt_trigger_message_false);
+        client.end();
+        return;
       }
-      client.end();
     });
   }
 
